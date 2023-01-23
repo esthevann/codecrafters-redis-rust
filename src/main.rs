c271d1d97d85903bc42f51773cd0e1424f46f4dd
esthevann <esthevanferretti69@gmail.com>
2023-01-23 20:34:57 -0300

    part 4 1st try

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-use std::{net::{TcpListener, TcpStream}, io::Write};
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                send_response(stream);
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
+
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    send_response(stream);
+                    println!("accepted new connection");
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
@@ -23,5 +28,4 @@ fn send_response(mut stream: TcpStream) {
     if let Err(e) = stream.write_all(b"+PONG\r\n") {
         println!("Couldn't send response {e}");
     }
-    
 }

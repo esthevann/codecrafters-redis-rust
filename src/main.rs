f3c09c881d24932a82d084c26c9b39972b10bff0
esthevann <esthevanferretti69@gmail.com>
2023-01-23 21:11:24 -0300

    part 4 6th try

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,18 @@ use std::{
     thread,
 };
 
+
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
+    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 thread::spawn(|| send_response(stream));
-                println!("accepted new connection");
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -24,7 +25,8 @@ fn main() {
 }
 
 fn send_response(mut stream: TcpStream) {
-    if let Err(e) = stream.write(b"+PONG\r\n") {
-        println!("Couldn't send response {e}");
+
+    while let Ok(_) = stream.write_all(b"+PONG\r\n") {
+        println!("Success");
     }
 }

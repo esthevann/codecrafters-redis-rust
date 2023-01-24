fbe5ce664edd57bbe0c9e0912ae0c4864a86dfc6
esthevann <esthevanferretti69@gmail.com>
2023-01-23 21:17:38 -0300

    part 4 9th try

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use std::{
-    io::Write,
+    io::{Write, Read},
     net::{TcpListener, TcpStream},
     thread,
 };
 
-
-
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-    
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
@@ -25,8 +23,14 @@ fn main() {
 }
 
 fn send_response(mut stream: TcpStream) {
-
-    while let Ok(_) = stream.write_all(b"+PONG\r\n") {
-        stream.flush().unwrap();
+    let mut buf = [0; 512];
+    loop {
+        // Wait for the client to send us a message but ignore the content for now
+        let bytes_read = stream.read(&mut buf).unwrap();
+        if bytes_read == 0 {
+            println!("client closed the connection");
+            break;
+        }
     }
+    stream.write_all(b"+PONG\r\n").unwrap();
 }
